e8fc52f406d0ba995074e1d54a0d979257dae41c
gregbakerstl <gregbakerstl@gmail.com>
2019-08-30 13:54:34 +0000

    sync

--- tool_solar_flux.sh
+++ tool_solar_flux.sh
@@ -5,18 +5,10 @@ solar_flux_data_file='solar_flux.csv'
 
 #curl "$api_endpoint"|grep -v '99999'|grep -v [a-z]|sed 's/,/ /g' > "${solar_flux_data_file}"
 
-data=$(cat ${solar_flux_data_file})
+data=$(cat ${solar_flux_data_file}|tail -1000)
 
 months=$(echo "$data"|awk {'print $2'}|sort|uniq)
 days=$(echo "$data"|awk {'print $3'}|sort|uniq)
-
-
-#solar_flux_all_days=$(echo "$data"|grep " $month_arg $day_arg"|awk {'print $4'})
-#solar_flux_all_days_sum=$(echo "$solar_flux_all_days"|awk '{s+=$1} END {print s}')
-#solar_flux_all_days_count=$(echo $solar_flux_all_days|wc -w)
-#solar_flux_all_days_average=$(echo "$solar_flux_all_days_sum / $solar_flux_all_days_count"|bc -l)
-
-echo "$solar_flux_all_days_average for month: $month_arg on day: $day_arg"
 
 get_solar_flux_by_date() {
 	month_arg=$1
@@ -25,6 +17,7 @@ get_solar_flux_by_date() {
 		grep " $month_arg $day_arg"|
 		awk {'print $4'}
 }
+
 
 get_average() {
 	local list="$@"
@@ -38,6 +31,7 @@ get_average() {
 for month in $months; do
 	for day in $days; do
 		result=$(get_average $(get_solar_flux_by_date $month $day))
-		echo "average solar_flux from 1947 to 2019 on $month/$day is $result"
+		#echo "average solar_flux on $month/$day is $result"
+		echo "$result $month $day"
 	done
-done
+done|sort -n|head -10
